1ef325265065fac83a93df8077aeb4f9097cb8ec
Vityuk Yuriy <you.thunderstruck@gmail.com>
2021-02-14 00:51:33 +0200

    добавялет фунционал обратного счетчика

--- src/index.js
+++ src/index.js
@@ -1,68 +1,98 @@
-/*Таймер обратного отсчета
-Создай плагин настраиваемого таймера, который ведет обратный отсчет до предварительно определенной даты. Такой плагин может использоваться в блогах и интернет-магазинах, 
-страницах регистрации событий, во время технического обслуживания и т. д.*/
-/*Плагин ожидает следующую HTML-разметку и показывает четыре цифры: дни, 
-часы, минуты и секунды в формате XX:XX:XX:XX. Количество дней может состоять из более чем двух цифр.*/
+/*refs = {
+  days: document.querySelector("[data-value='days']"),
+  hours: document.querySelector("[data-value='hours']"),
+  mins: document.querySelector("[data-value='mins']"),
+  secs: document.querySelector("[data-value='secs']"),
+};
 
-<div class="timer" id="timer-1">
-  <div class="field">
-    <span class="value" data-value="days">
-      11
-    </span>
-    <span class="label">Days</span>
-  </div>
+const targetDate = new Date('Feb 15, 2021');
+const targetDateMs = Date.parse(targetDate);
+/*const currentDateMs = Date.now();
+const differenceInTime = targetDateMs - currentDateMs;*/
+/*
+function converTimer(differenceInTime) {
+  refs.days.textContent = pad(Math.floor(differenceInTime / (1000 * 60 * 60 * 24)));
+  refs.hours.textContent = pad(Math.floor((differenceInTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  refs.mins.textContent = pad(Math.floor((differenceInTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  refs.secs.textContent = pad(Math.floor((differenceInTime % (1000 * 60)) / 1000));
+}
 
-  <div class="field">
-    <span class="value" data-value="hours">
-      11
-    </span>
-    <span class="label">Hours</span>
-  </div>
+function pad(value) {
+  return String(value).padStart(2, '0');
+}
 
-  <div class="field">
-    <span class="value" data-value="mins">
-      11
-    </span>
-    <span class="label">Minutes</span>
-  </div>
+function countdownTimer() {
+  const currentDateMs = Date.now();
+  const differenceInTime = targetDateMs - currentDateMs;
+  converTimer(differenceInTime);
+}
 
-  <div class="field">
-    <span class="value" data-value="secs">
-      11
-    </span>
-    <span class="label">Seconds</span>
-  </div>
-</div>;
+const timerId = setInterval(countdownTimer, 1000);
+*/
 
-/*Плагин это класс CountdownTimer, экземпляр которого создает новый таймер с настройками.*/
-new CountdownTimer({
-  selector: "#timer-1",
-  targetDate: new Date("Jul 17, 2019"),
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.root = document.querySelector(this.selector);
+    this.targetDate = targetDate;
+
+    this.refs = {
+      days: this.root.querySelector("[data-value='days']"),
+      hours: this.root.querySelector("[data-value='hours']"),
+      mins: this.root.querySelector("[data-value='mins']"),
+      secs: this.root.querySelector("[data-value='secs']"),
+    };
+    this.intervalId = null;
+  }
+
+  updateTime(differenceInTime) {
+    this.refs.days.textContent = this.pad(Math.floor(differenceInTime / (1000 * 60 * 60 * 24)));
+    this.refs.hours.textContent = this.pad(Math.floor((differenceInTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    this.refs.mins.textContent = this.pad(Math.floor((differenceInTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    this.refs.secs.textContent = this.pad(Math.floor((differenceInTime % (1000 * 60)) / 1000));
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  start() {
+    if (this.intervalId != null) {
+      return;
+    }
+    const targetDateInMs = Date.parse(this.targetDate);
+    this.intervalId = setInterval(() => {
+      const currentDateMs = Date.now();
+      const deltaTime = targetDateInMs - currentDateMs;
+
+      if (deltaTime > 0) {
+        this.updateTime(deltaTime);
+      } else {
+        return;
+      }
+    }, 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+  }
+  clear() {
+    clearInterval(this.intervalId);
+    this.refs.days.textContent = this.pad(0);
+    this.refs.hours.textContent = this.pad(0);
+    this.refs.mins.textContent = this.pad(0);
+    this.refs.secs.textContent = this.pad(0);
+    this.intervalId = null;
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Feb 17, 2021'),
 });
 
-/*Для подсчета значений используй следующие готовые
- формулы, где time - разница между targetDate и текущей датой.*/
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+timer.start();
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const timer2 = new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('Mar 17, 2021'),
+});
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+timer2.start();
